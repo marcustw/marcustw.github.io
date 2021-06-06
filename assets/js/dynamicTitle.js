6e6934aa225c2eb042fff04b002a799779926ac4
MarcusTw <60913700+MarcusTw@users.noreply.github.com>
2021-06-06 20:58:47 +0800

    Add my favourite coin as dp

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -18,7 +18,7 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "./assets/images/dp_male.svg";
+      favicon.href = "./assets/images/dp_doge.jpg";
     }
   });
 
@@ -26,7 +26,7 @@ window.onload = function () {
     blinkEvent = setInterval(() => {
       if (document.title === attentionMessage) {
         document.title = pageTitle;
-        favicon.href = "./assets/images/dp_male.svg";
+        favicon.href = "./assets/images/dp_doge.jpg";
       } else {
         document.title = attentionMessage;
         favicon.href = "./assets/images/folded.png";
