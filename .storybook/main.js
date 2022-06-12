c5c00547f037df070eaba9428b156a17c6d0df65
marcustw <e0014884@u.nus.edu.sg>
2022-06-12 23:38:15 +0800

    feat: add CI pipeline for storybook web

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   typescript: {
-    check: true, // type-check stories during Storybook build
-  }
+    check: true,
+  },
 };
