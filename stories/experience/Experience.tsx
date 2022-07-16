c60f46450d26836ff996efdc2714ab958d6600e9
marcustw <e0014884@u.nus.edu>
2022-07-17 02:55:13 +0800

    feat: update about me page

---
+++ stories/experience/Experience.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Experience = ({ ...args }) => {
+  return <div>{"Hello World"}</div>
+};
+
+export default Experience;
---
+++ stories/projects/Project.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Project = ({ ...args }) => {
+  return <div>{"Hello World"}</div>
+};
+
+export default Project;
